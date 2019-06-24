f2fba83b0bb41eaaa9ebcd6d1cf3f193ce14c90a
Evan Goode <mail@evangoo.de>
2019-06-23 22:28:30 -0400

    Support arbitrary configurations of sensors and songs

--- singsparrow/singsparrow.py
+++ singsparrow/singsparrow.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""SingSparrow II operant conditioning software"""
+
 from collections import namedtuple
 import csv
 from datetime import datetime
@@ -13,32 +15,28 @@ import toml
 
 CONFIG_PATH = "/etc/singsparrow-ii.toml"
 OUTPUT_DIRECTORY = "/var/log/singsparrow-ii/"
-LOG_EXTENSION = "tsv"
-LOG_DELIMITER = "\t"
 
-SIDE_A = "a"
-SIDE_B = "b"
-SIDE_NEITHER = "neither"
-
-
-Press = namedtuple("Press", ("when", "key", "song_played"))
+Event = namedtuple("Event", ("when", "sensor", "song_played"))
 
 
 def other_side(side):
-    return SIDE_A if side == SIDE_B else SIDE_B
+    """In the case that there are two sensors and two songs, return the sensor
+    or song on the other side"""
+    return int(not side)
 
 
-def simple_quota_schedule(config, key, when, history):
+def simple_quota_schedule(_config, key, _when, history):
     """If the quota has been reached, play nothing. Otherwise, play the
     associated song."""
     quota = 50
 
     spent = sum(1 for press in history if press.song_played == key)
-    print("spent is: ", spent)
-    return key if spent < quota else SIDE_NEITHER
+
+    print("spent:", spent)
+    return key if spent < quota else None
 
 
-def self_balancing_schedule(config, key, history):
+def self_balancing_schedule(_config, key, history):
     """Attempt to balance the number of plays of each song while maintaining
     the associations between key and song"""
     quota = 50
@@ -46,10 +44,12 @@ def self_balancing_schedule(config, key, history):
     maximum_odds = 3 / 4
     minimum_odds = 1 / 2
 
-    def compute_odds(x, a, b, halfway):
-        """Exponentially decay from `a`, asymptotically to `b`, crossing the
-        arithmetic mean of `a` and `b` at x = `halfway`"""
-        return ((a - b) / (2 ** (x / halfway))) + b
+    def compute_odds(value, maximum, minimum, halfway):
+        """Exponentially decay from `maximum`, asymptotically to `minimum`,
+        crossing the arithmetic mean of `maximum` and `minimum` at value =
+        `halfway`"""
+        difference = maximum - minimum
+        return (difference / (2 ** (value / halfway))) + minimum
 
     other = other_side(key)
 
@@ -57,7 +57,7 @@ def self_balancing_schedule(config, key, history):
     other_plays = sum(1 for press in history if press.song_played == other)
 
     if key_plays >= quota and other_plays >= quota:
-        return SIDE_NEITHER
+        return None
     if key_plays >= quota:
         return other
     if other_plays >= quota:
@@ -73,60 +73,78 @@ def self_balancing_schedule(config, key, history):
 
 
 def get_day(when):
+    """Format a datetime `when` as an ISO 8601 date"""
     return when.strftime("%Y-%m-%d")
 
 
 def get_log_path(when):
+    """Given a datetime `when`, get the path to the appropriate log file"""
     os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
-    return os.path.join(OUTPUT_DIRECTORY, f"{get_day(when)}.{LOG_EXTENSION}")
+    return os.path.join(OUTPUT_DIRECTORY, f"{get_day(when)}.tsv")
 
 
-def log_press(config, press):
-    path = get_log_path(press.when)
+def get_label(the_item, items):
+    """Given the index of a sensor or song, return its label"""
+    if the_item is None:
+        return repr(the_item)
+    try:
+        return items[the_item]["label"]
+    except IndexError as error:
+        raise RuntimeError(f"") from error
+
+
+def unlabel(the_label, items):
+    """Given the label of a sensor or song, return its index"""
+    if the_label == repr(None):
+        return None
+    for index, item in enumerate(items):
+        if item["label"] == the_label:
+            return index
+    raise RuntimeError(
+        f'Couldn\'t tell what "{the_label}" refers to. '
+        "Corrupt log, or maybe labels were changed in {CONFIG_PATH}?"
+    )
+
+
+def log_event(config, event):
+    """Log an event to the appropriate TSV file"""
+    path = get_log_path(event.when)
     existed_already = os.path.isfile(path)
     with open(path, "a") as log_file:
         writer = csv.DictWriter(
-            log_file,
-            delimiter=LOG_DELIMITER,
-            fieldnames=["timestamp", "key", "song-played"],
+            log_file, delimiter="\t", fieldnames=["timestamp", "sensor", "song-played"]
         )
         if not existed_already:
             writer.writeheader()
         writer.writerow(
             {
-                "timestamp": press.when.timestamp(),
-                "key": config["key-labels"][press.key],
-                "song-played": config["song-labels"][press.song_played],
+                "timestamp": event.when.timestamp(),
+                "sensor": get_label(event.sensor, config["sensor"]),
+                "song-played": get_label(event.song_played, config["song"]),
             }
         )
 
 
 def parse_log_row(config, row):
-    def unlabel(the_label, labels):
-        for side, label in labels.items():
-            if label == the_label:
-                return side
-        raise ValueError(
-            f'Couldn\'t tell which side "{the_label}" was on. '
-            "Corrupt log, or maybe labels were changed in {CONFIG_PATH}?"
-        )
-
+    """Parse an Event from one row of a log file"""
     when = datetime.fromtimestamp(float(row["timestamp"]))
-    key = unlabel(row["key"], config["key-labels"])
-    song_played = unlabel(row["song-played"], config["song-labels"])
-    return Press(when, key, song_played)
+    sensor = unlabel(row["sensor"], config["sensor"])
+    song_played = unlabel(row["song-played"], config["song"])
+    return Event(when, sensor, song_played)
 
 
 def parse_log(config, when):
+    """Read Events from earlier in the day"""
     path = get_log_path(when)
     if not os.path.isfile(path):
         return []
     with open(path, "r") as log_file:
-        reader = csv.DictReader(log_file, delimiter=LOG_DELIMITER)
+        reader = csv.DictReader(log_file, delimiter="\t")
         return [parse_log_row(config, row) for row in reader]
 
 
 def verify_readable(path):
+    """Raise an error if `path` is unreadable"""
     try:
         open(path).close()
     except IOError as error:
@@ -134,6 +152,7 @@ def verify_readable(path):
 
 
 def verify_writable(path):
+    """Raise an error if `path` is unwritable"""
     existed_already = os.path.isfile(path)
     try:
         Path(path).touch(exist_ok=True)
@@ -144,57 +163,45 @@ def verify_writable(path):
 
 
 def read_config():
-    def verify_path(config, *path):
-        for key in path:
-            try:
-                config = config[key]
-            except KeyError as error:
-                message = f'Couldn\'t find option "{".".join(path)} in config file!'
-                raise RuntimeError(message) from error
-
+    """Read and validate the TOML config file"""
     with open(CONFIG_PATH, "r") as config_file:
         config = toml.load(config_file)
-
-        # get the KeyErrors out of the way before we get going
-        for side in ("a", "b"):
-            verify_path(config, "key-labels", side)
-            verify_path(config, "key-pins", side)
-            verify_path(config, "song-labels", side)
-            verify_path(config, "song-paths", side)
-            verify_readable(config["song-paths"][side])
-        verify_path(config, "song-labels", "neither")
-        return config
+    for song in config["song"]:
+        verify_readable(song["path"])
+    return config
 
 
 def play_song(config, song):
-    subprocess.check_call(["aplay", config["song-paths"][song]])
+    """Play a song given its index"""
+    subprocess.check_call(["aplay", config["song"][song]["path"]])
 
 
 def main():
+    """Entry point"""
     config = read_config()
     verify_writable(get_log_path(when=datetime.now()))
     history = parse_log(config, datetime.now())
-    print(f"Read {len(history)} presses from history.")
+    print(f"Read {len(history)} events from history.")
 
-    def handle_key_press(key_pressed):
+    def handle_sensor(sensor):
         now = datetime.now()
         if history and get_day(now) != get_day(history[-1].when):
             print("It's a new day, clearing history...")
             history.clear()
 
-        song = simple_quota_schedule(config, key_pressed, now, history)
-        if song != SIDE_NEITHER:
+        song = simple_quota_schedule(config, sensor, now, history)
+        if song is not None:
             play_song(config, song)
-        press = Press(now, key=key_pressed, song_played=song)
-        print(f"Logging {press}...")
-        history.append(press)
-        log_press(config, press)
+        event = Event(now, sensor, song)
+        print(f"Logging {event}...")
+        history.append(event)
+        log_event(config, event)
 
     # simulations TODO
     time.sleep(1)
     for _ in range(30):
-        handle_key_press(SIDE_A)
-        handle_key_press(SIDE_B)
+        handle_sensor(0)
+        handle_sensor(1)
 
 
 if __name__ == "__main__":
